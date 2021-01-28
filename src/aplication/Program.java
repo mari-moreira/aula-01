e6794ddf1bc64b9a43a33f5ab310f7041a279744
mari-moreira <moreiramarimana137@gmail.com>
2021-01-28 00:40:21 -0300

    Adicionada mensagem de boa noite e formatacao

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -2,9 +2,10 @@ package aplication;
 
 public class Program {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		System.out.println("Bom dia ");
 		System.out.println("Boa tarde");
+		System.out.println("Boa noite");
 
 	}
 
